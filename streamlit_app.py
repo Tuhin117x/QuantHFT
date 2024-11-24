d232a9d1213c634c57360716d34ab6022c4f1265
Tuhin117x <92184005+Tuhin117x@users.noreply.github.com>
2024-11-24 13:35:35 +0000

    new version

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
+import pandas as pd
+from datetime import datetime
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! "
-)
+st.sidebar.image("logo.jpg")
+
+st.sidebar.write('--------------')
+
+strategies=['Momentum Strategy - Type 1','Momentum Strategy - Type 2']
+# Add a selectbox to the sidebar:
+strategy_selectbox_side = st.sidebar.selectbox('Select your Quant Strategy', (strategies))
+
+
+
+
